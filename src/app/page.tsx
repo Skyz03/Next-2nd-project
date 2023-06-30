f5eb59c01114ab93ce313e2ddbef2044afecdbe5
Skyz03 <aakibdiscoo@gmail.com>
2023-06-30 20:49:36 +0545

    fix: update with the images & style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
+import Image from "next/image";
 interface AdviceSlip {
   slip: {
     id: number;
@@ -27,12 +27,24 @@ function Home() {
   }, []);
 
   return (
-    <div className="bg-darkblue text-center h-[100vh] w-full">
+    <div className="bg-darkblue font-manrope text-center h-[100vh] w-full">
       {advice && (
         <div className="flex h-full justify-center items-center">
           <div className="container w-auto flex flex-col items-center justify-center bg-grayishblue p-4 rounded-lg ">
-            <p className="text-neongreen">ADIVCE # {advice.slip.id}</p>
-            <p>&quot;{advice.slip.advice}&quot;</p>
+            <p className="text-neongreen text-[9px] tracking-[0.15rem]">
+              ADIVCE # {advice.slip.id}
+            </p>
+            <p className="text-white text-xl mt-4 text font-semibold tracking-wide">
+              &quot;{advice.slip.advice}&quot;
+            </p>
+            <Image
+              className="mt-6"
+              src="/pattern-divider-desktop.svg"
+              alt="me"
+              width="100"
+              height="64"
+              style={{ width: "100%" }}
+            />
           </div>
         </div>
       )}
