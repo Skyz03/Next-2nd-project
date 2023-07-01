7ee69c3d9f1dd82eab6773fa39695780517716a9
Skyz03 <aakibdiscoo@gmail.com>
2023-07-01 11:24:00 +0545

    fix: added useeffect with click events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,42 +13,62 @@ function Home() {
   const [advice, setAdvice] = useState<AdviceSlip | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://api.adviceslip.com/advice");
-        const data: AdviceSlip = await response.json();
-        setAdvice(data);
-      } catch (error) {
-        console.error("Error fetching advice:", error);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data: AdviceSlip = await response.json();
+      setAdvice(data);
+    } catch (error) {
+      console.error("Error fetching advice:", error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    fetchData();
+  };
+
   return (
-    <div className="bg-darkblue font-manrope text-center h-[100vh] w-full">
-      {advice && (
-        <div className="flex h-full justify-center items-center">
-          <div className="container w-auto flex flex-col items-center justify-center bg-grayishblue p-8 rounded-lg ">
-            <p className="text-neongreen text-[9px] tracking-[0.15rem]">
-              ADIVCE # {advice.slip.id}
-            </p>
-            <p className="text-white text-xl mt-4 text font-semibold tracking-wide">
-              &quot;{advice.slip.advice}&quot;
-            </p>
-            <Image
-              className="mt-6"
-              src="/pattern-divider-desktop.svg"
-              alt="me"
-              width="100"
-              height="64"
-              style={{ width: "100%", maxWidth: "700px" }}
-            />
+    <>
+      <div className="bg-darkblue font-manrope text-center h-[100vh] w-full">
+        {advice && (
+          // This is the body elements start
+          <div className="flex h-full justify-center items-center flex-col">
+            {/* This is the card start  */}
+            <div className="container w-auto flex flex-col items-center justify-center bg-grayishblue p-8 rounded-lg ">
+              <p className="text-neongreen text-[9px] tracking-[0.15rem]">
+                ADIVCE # {advice.slip.id}
+              </p>
+              <p className="text-white text-xl mt-4 text font-semibold tracking-wide">
+                &quot;{advice.slip.advice}&quot;
+              </p>
+              <Image
+                className="mt-6"
+                src="/pattern-divider-desktop.svg"
+                alt="me"
+                width="100"
+                height="64"
+                style={{ width: "100%", maxWidth: "700px" }}
+              />
+            </div>
+            {/* This is the card end  */}
+            <div className="">
+              <Image
+                className=""
+                src="/icon-dice.svg"
+                alt="me"
+                width="100"
+                height="10"
+                onClick={handleButtonClick}
+              />
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+          // This is the body elements end
+        )}
+      </div>
+    </>
   );
 }
 
